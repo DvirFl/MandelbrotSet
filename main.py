9835ecc87f6d674112cb77415eb2046fd8f19465
tamirgr <tamirgr@post.bgu.ac.il>
2024-02-17 22:09:29 +0200

    first iteration

---
+++ main.py
@@ -0,0 +1,63 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GL.shaders import compileProgram, compileShader
+
+# Vertex Shader
+VERTEX_SHADER = """
+#version 330
+layout(location = 0) in vec3 position;
+void main() {
+    gl_Position = vec4(position, 1.0);
+}
+"""
+
+# Fragment Shader
+FRAGMENT_SHADER = """
+#version 330
+out vec4 fragColor;
+void main() {
+    fragColor = vec4(1.0, 0.0, 0.0, 1.0);  // Red color
+}
+"""
+
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    # Compile shaders and program
+    shaderProgram = compileProgram(
+        compileShader(VERTEX_SHADER, GL_VERTEX_SHADER),
+        compileShader(FRAGMENT_SHADER, GL_FRAGMENT_SHADER)
+    )
+
+    # Define vertices and buffer
+    vertices = [
+        -0.5, -0.5, 0.0,
+         0.5, -0.5, 0.0,
+         0.0,  0.5, 0.0
+    ]
+    vertex_buffer = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
+    glBufferData(GL_ARRAY_BUFFER, (GLfloat * len(vertices))(*vertices), GL_STATIC_DRAW)
+
+    # Set the position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+    glEnableVertexAttribArray(0)
+
+    # Main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glUseProgram(shaderProgram)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+if __name__ == '__main__':
+    main()
